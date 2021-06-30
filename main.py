b9ab50a41be8013f35721cdcfc66f55006415a12
Rahber Abbas Abidi <rahberabbasabidi@Rahbers-MacBook-Air.local>
2021-06-30 12:24:57 +0530

    Initial commit14

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ options.headless = True
 options.add_argument('window-size=1400,600')
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
 options.add_experimental_option('useAutomationExtension', False)
+options.add_argument(f'user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36')
 driver = webdriver.Chrome(options=options)
 driver.get("https://snaptik.app/")
 print(driver.title)
