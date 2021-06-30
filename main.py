a3a6a9fb281a7fd53ca01e35d0a60bda2f9df526
Rahber Abbas Abidi <rahberabbasabidi@Rahbers-MacBook-Air.local>
2021-06-30 13:09:16 +0530

    Initial commit16

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
 from selenium import webdriver
+import time
+from moviepy.editor import *
 from selenium.webdriver.chrome.options import Options
-options = Options()
-options.headless = True
-driver = webdriver.Chrome(options=options)
-driver.get("https://qload.info/")
-print(driver.title)
-driver.quit()
+
+headers = {
+        "method": "GET",
+        "accept-encoding": "utf-8",
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
+    }
+
+def without(url):
+    options = Options()
+    options.headless = True
+    options.add_argument('window-size=1400,600')
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('useAutomationExtension', False)
+    driver = webdriver.Chrome(options=options)
+    driver.get('https://snaptik.app/')   
+    time.sleep(2)
+    input = driver.find_element_by_id('link')
+    input.send_keys(url)
+    time.sleep(2)
+    driver.find_element_by_id('submit').click()
+    time.sleep(1)
+    elms = driver.find_elements_by_xpath("//a[@href]")
+    lst = []
+    for elem in elms:
+        rtr = elem.get_attribute("href")
+        lst.append(rtr)
+        # print(rtr)
+    # link =  lst[3]
+    print(lst)
+
+without('https://www.tiktok.com/@argenis_1999/video/6967870054994087174?sender_device=pc&sender_web_id=6969977128956642817&is_from_webapp=v1&is_copy_url=0')
