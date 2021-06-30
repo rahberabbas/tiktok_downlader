03f4a1fb60034fbe0c81c4599a4b34a553753636
Rahber Abbas Abidi <rahberabbasabidi@Rahbers-MacBook-Air.local>
2021-06-30 13:36:42 +0530

    latest

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ headers = {
         "method": "GET",
         "accept-encoding": "utf-8",
         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
+        
     }
 
 def without(url):
