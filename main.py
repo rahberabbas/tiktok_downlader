877cf8bb4640d8c4e67c88130f84b2d870641a88
Rahber Abbas Abidi <rahberabbasabidi@Rahbers-MacBook-Air.local>
2021-06-30 12:19:37 +0530

    Initial commit10

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
-import requests
-import json
+from requests.api import head
+from selenium import webdriver
+import time
+from moviepy.editor import *
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
-def downloader(link):
+headers = {
+        "method": "GET",
+        "accept-encoding": "utf-8",
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
+    }
+
+def without(url):
+    options = Options()
+    options.headless = True
+    options.add_argument('window-size=1400,600')
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('useAutomationExtension', False)
+    driver = webdriver.Chrome(options=options)
+    driver.get('https://snaptik.app/')   
+    input = driver.find_element_by_id('url')
+    input.send_keys(url)
+    time.sleep(2)
+    driver.find_element_by_id('submiturl').click()
+    time.sleep(5)
+    elms = driver.find_elements_by_xpath("//a[@href]")
+    lst = []
+    for elem in elms:
+        rtr = elem.get_attribute("href")
+        lst.append(rtr)
+        # print(rtr)
+    link =  lst[3]
     print(link)
-    # newHeaders = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-    source = "https://freevideosdowloader.tk/services/downloader_api.php"
-    resp = requests.post(source, data={"url": link}, verify=True).text
-    print(resp)
-    links_list = json.loads(resp)["VideoResult"][0]["VideoUrl"]
-    print(links_list)
 
-downloader(link='https://www.tiktok.com/@amg_65s_bro/video/6970634217176714502')
+without('https://www.tiktok.com/@argenis_1999/video/6967870054994087174?sender_device=pc&sender_web_id=6969977128956642817&is_from_webapp=v1&is_copy_url=0')
