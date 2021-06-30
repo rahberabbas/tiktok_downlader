2957a7cd730dff59ea335802ac110dfe1fb8afc1
Rahber Abbas Abidi <rahberabbasabidi@Rahbers-MacBook-Air.local>
2021-06-30 12:35:46 +0530

    Initial commit16

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import requests
-headers = {
-        "method": "GET",
-        "accept-encoding": "utf-8",
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
-    }
-r = requests.get('https://qload.info/', headers=headers)
-print(r.status_code)
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+options = Options()
+options.headless = True
+driver = webdriver.Chrome("/usr/bin/chromedriver", options=options)
+driver.get("https://qload.info/")
+print(driver.title)
+driver.quit()
