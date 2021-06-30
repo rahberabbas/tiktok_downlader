379d6c931ea6eaa2b7d1f0e12d23599d62b201a2
Rahber Abbas Abidi <rahberabbasabidi@Rahbers-MacBook-Air.local>
2021-06-30 13:33:13 +0530

    latest

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from requests.api import head
 from selenium import webdriver
 import time
 from moviepy.editor import *
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 
 headers = {
@@ -16,20 +18,15 @@ def without(url):
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
     options.add_experimental_option('useAutomationExtension', False)
     driver = webdriver.Chrome(options=options)
-    driver.get('https://snaptik.app/')   
-    time.sleep(2)
-    input = driver.find_element_by_id('link')
-    input.send_keys(url)
-    time.sleep(2)
-    driver.find_element_by_id('submit').click()
-    time.sleep(1)
+    driver.get(f'https://ttdownloader.com/?url={url}')
+    time.sleep(7)
     elms = driver.find_elements_by_xpath("//a[@href]")
     lst = []
     for elem in elms:
         rtr = elem.get_attribute("href")
         lst.append(rtr)
         # print(rtr)
-    # link =  lst[3]
-    print(lst)
+        print(rtr)
+
 
 without('https://www.tiktok.com/@argenis_1999/video/6967870054994087174?sender_device=pc&sender_web_id=6969977128956642817&is_from_webapp=v1&is_copy_url=0')
