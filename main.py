a370d14a83e2602832552e8b245df9080c18e7ee
Rahber Abbas Abidi <rahberabbasabidi@Rahbers-MacBook-Air.local>
2021-06-30 12:30:40 +0530

    Initial commit16

--- main.py
+++ main.py
@@ -1,21 +1,8 @@
-from selenium import webdriver
-from pyvirtualdisplay import Display
-from selenium.webdriver.chrome.options import Options
+import requests
 headers = {
         "method": "GET",
         "accept-encoding": "utf-8",
         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
     }
-display = Display(visible=0, size=(1024, 768))
-display.start()
-options = Options()
-options.headless = True
-options.add_argument("--incognito")
-# options.add_argument('window-size=1400,600')
-options.add_experimental_option("excludeSwitches", ["enable-automation"])
-options.add_experimental_option('useAutomationExtension', False)
-options.add_argument(f'user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36')
-driver = webdriver.Chrome(options=options)
-driver.get("https://snaptik.app/")
-print(driver.title)
-driver.quit()
+r = requests.get('https://snaptik.app/', headers=headers)
+print(r.status_code)
