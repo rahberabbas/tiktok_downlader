520f363945928fe03d7c41b62ae9eb1a97f17241
rahberabbas <amanabidi1234@gmail.com>
2021-06-30 22:48:01 +0530

    laster2

--- tiktok/settings.py
+++ tiktok/settings.py
@@ -131,12 +131,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static_old")]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static_files'),
-# ]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
