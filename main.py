e5e2fe9231d1f40ac1c8fe4e77af79034cb8a830
Rahber Abbas Abidi <rahberabbasabidi@Rahbers-MacBook-Air.local>
2021-06-30 12:36:16 +0530

    Initial commit16

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 options = Options()
 options.headless = True
-driver = webdriver.Chrome("/usr/bin/chromedriver", options=options)
+driver = webdriver.Chrome(options=options)
 driver.get("https://qload.info/")
 print(driver.title)
 driver.quit()
