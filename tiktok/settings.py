8e624f1ec925753d094bf7064b0c038f34617885
rahberabbas <amanabidi1234@gmail.com>
2021-07-01 01:13:41 +0530

    laster2

--- tiktok/settings.py
+++ tiktok/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-v+4c#f839=^#lp!ltrmc+ne++0%xztt7i@=ui%4hun9t1()_#s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['157.245.93.208', '127.0.0.1']
+ALLOWED_HOSTS = ['157.245.93.208', '127.0.0.1', 'tiksss.com', 'www.tiksss.com']
 
 
 # Application definition
