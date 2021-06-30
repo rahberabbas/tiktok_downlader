7091326113fb5d49b945ff64e882458495026a04
rahberabbas <amanabidi1234@gmail.com>
2021-06-30 20:59:44 +0530

    laster2

--- tiktok/settings.py
+++ tiktok/settings.py
@@ -132,9 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static_files'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static_files'),
+# ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
