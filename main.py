5643b23edba2a7286ca2cbff71ed87afcdac3dab
Rahber Abbas Abidi <rahberabbasabidi@Rahbers-MacBook-Air.local>
2021-06-30 12:32:04 +0530

    Initial commit16

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ headers = {
         "accept-encoding": "utf-8",
         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
     }
-r = requests.get('https://snaptik.app/', headers=headers)
+r = requests.get('https://qload.info/', headers=headers)
 print(r.status_code)
