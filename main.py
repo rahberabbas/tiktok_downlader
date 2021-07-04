b879c5a814f135f79ea45fcfcaec414116dede39
rahberabbas <amanabidi1234@gmail.com>
2021-07-04 14:17:56 +0530

    lets

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import pyshorteners
+from datetime import datetime
+import uuid
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-link = 'https://ttdownloader.com/dl.php?v=YTo0OntzOjk6IndhdGVybWFyayI7YjowO3M6NzoidmlkZW9JZCI7czozMjoiN2QzNjUxYjNkODFkY2E2YTBiMjQ5YWE0ODdhZjk1YWEiO3M6MzoidWlkIjtzOjMyOiI1YmZlNTJjYmZkNGI3YzQ0NWYzZTBlMGI2YTY1M2M5YyI7czo0OiJ0aW1lIjtpOjE2MjUyNTc5NjQ7fQ=='
-
-short = pyshorteners.Shortener()
-
-x = short.tinyurl.short(link)
-
-print(x)
+filename = str(uuid.uuid4())
+print(os.path.join(BASE_DIR+"/video_down",filename))
