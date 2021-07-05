0097e5784290c615ac044325c2363f4445f52be1
rahberabbas <amanabidi1234@gmail.com>
2021-07-05 11:46:39 +0530

    lets

--- without/views.py
+++ without/views.py
@@ -19,10 +19,22 @@ def download2(request):
         if "tiktok.com" in url:
             url = url
             rtr = functions.without(url=url)
+            r = uuid.uuid4()
+            file_hello = f"tiksss.com_{r}.mp4"
+            data = rtr.content
+            filename = str(uuid.uuid4())
+
+            with open(os.path.join(BASE_DIR+"/without_video",filename+'.mp4'), 'wb') as f:
+                f.write(data)
+
+            with open(os.path.join(BASE_DIR+"/without_video",filename+'.mp4'), 'rb') as f:
+                data = f.read()
+
+            response = HttpResponse(data, content_type='application/vnd.mp4')
+            response['Content-Disposition'] = "attachment; filename=%s" % file_hello
+            return response
         else:
-            return HttpResponse("Your link is Invalid")
-        context={'url': url}
-        return render(request, 'download2.html', context)
+            return render(request, 'without.html')
     else:
         return render(request, 'without.html')
 
