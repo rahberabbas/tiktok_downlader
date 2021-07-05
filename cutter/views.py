7c84fc971cf3f0809f3b742efe0f8d32b2acff50
rahberabbas <amanabidi1234@gmail.com>
2021-07-05 11:51:41 +0530

    lets

--- cutter/views.py
+++ cutter/views.py
@@ -42,8 +42,35 @@ def download4(request):
         tt2 = int(t2)
         pick = request.POST.get('pick')
         context = {'t1': tt1, 't2': tt2, 'rtr': rtr, 'pick': pick}
-        print(context)
-        return render(request, 'download4.html', context)
+        if rtr and pick == "mp4":
+            r = uuid.uuid4()
+            file_hello = f"tiksss.com_{r}.mp4"
+            filename = datetime.strftime(datetime.now(), '%Y-%m-%d-%H-%M-%S')
+            ffmpeg_extract_subclip(filename=rtr, t1=tt1, t2=tt2, targetname=(os.path.join(BASE_DIR+"/video_cut",filename) + '.mp4'))
+
+            with open(os.path.join(BASE_DIR+"/video_cut",filename+'.mp4'), 'rb') as f:
+                data = f.read()
+
+            response = HttpResponse(data, content_type='application/vnd.mp4')
+            response['Content-Disposition'] = "attachment; filename=%s" % file_hello
+            return response
+        elif rtr and pick == "mp3":
+            r = uuid.uuid4()
+            file_hello = f"tiksss.com_{r}.mp3"
+            filename = datetime.strftime(datetime.now(), '%Y-%m-%d-%H-%M-%S')
+            ffmpeg_extract_subclip(filename=rtr, t1=tt1, t2=tt2, targetname=(os.path.join(BASE_DIR+"/video_cut",filename) + '.mp4'))
+
+            clip = VideoFileClip(os.path.join(BASE_DIR+"/video_cut",filename+'.mp4'))
+            clip.audio.write_audiofile(os.path.join(BASE_DIR+"/audio_cut",filename+'.mp3'))
+
+            with open(os.path.join(BASE_DIR+"/audio_cut",filename+'.mp3'), 'rb') as f:
+                data = f.read()
+
+            response = HttpResponse(data, content_type='application/audio.mp3')
+            response['Content-Disposition'] = "attachment; filename=%s" % file_hello
+            return response
+        else:
+            return render(request, 'cutter.html')
     else:
         return render(request, 'download4.html')
 
