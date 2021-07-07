f9b526631c144aa9f19c35814b8623067ce3e901
rahberabbas <amanabidi1234@gmail.com>
2021-07-07 14:04:14 +0530

    last commit

--- audio_cutter/views.py
+++ audio_cutter/views.py
@@ -21,12 +21,29 @@ def download5(request):
         if "tiktok.com" in url:
             url = url
             rtr = functions.without(url=url)
+            t1 = request.POST.get('t1')
+            tt1 = int(t1)
+            t2 = request.POST.get('t2')
+            tt2 = int(t2)
+            rts = functions.without(url=url)
+            r = uuid.uuid4()
+            file_hello = f"tiksss.com_{r}.mp3"
+            filename = datetime.strftime(datetime.now(), '%Y-%m-%d-%H-%M-%S')
+            ffmpeg_extract_subclip(filename=rts, t1=tt1, t2=tt2, targetname=(os.path.join(BASE_DIR+"/video_cut",filename) + '.mp4'))
+
+            clip = VideoFileClip(os.path.join(BASE_DIR+"/video_cut",filename+'.mp4'))
+            clip.audio.write_audiofile(os.path.join(BASE_DIR+"/audio_cut",filename+'.mp3'))
+
+            with open(os.path.join(BASE_DIR+"/audio_cut",filename+'.mp3'), 'rb') as f:
+                data = f.read()
+
+            response = HttpResponse(data, content_type='application/audio.mp3')
+            response['Content-Disposition'] = "attachment; filename=%s" % file_hello
+            return response
         else:
-            return HttpResponse("Your link is Invalid")
-        context={'url': url, 'rtr': rtr}
-        return render(request, 'download5.html', context)
+            return render(request, 'audio_cutter.html')
     else:
-        return redirect(request, 'audio_cutter.html')
+        return render(request, 'audio_cutter.html')
 
 def download6(request):
     if request.method == "POST":
