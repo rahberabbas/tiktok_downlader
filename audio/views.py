a9fab8e33a86cf95b9684731290f721ee7312468
rahberabbas <amanabidi1234@gmail.com>
2021-07-21 22:39:00 +0530

    Last

--- audio/views.py
+++ audio/views.py
@@ -34,7 +34,7 @@ def download1(request):
             with open(os.path.join(BASE_DIR+"/audio_down2",filename+'.mp3'), 'rb') as f:
                 data = f.read()
             time.sleep(7)
-            response = HttpResponse(data, content_type='application/aud.mp3')
+            response = HttpResponse(data, content_type='audio/webm')
             response['Content-Disposition'] = "attachment; filename=%s"  % file_hello
             return response
         else:
