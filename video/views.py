c4cbaafc5e7ad58b0171519e0c6cf79865ffb5e6
rahberabbas <amanabidi1234@gmail.com>
2021-08-13 16:21:43 +0530

    commited

--- video/views.py
+++ video/views.py
@@ -11,7 +11,30 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    if request.method == "POST":
+        global url
+        global rtr
+        url = request.POST.get('url')
+        if "tiktok.com" in url:
+            url = url
+            rtr = function.withwater_download(urls=url)
+            r = uuid.uuid4().hex
+            file_hello = f"tiksss_{r}.mp4"
+            data = rtr.content
+            filename = str(uuid.uuid4())
+            with open(os.path.join(BASE_DIR+"/video_down",filename) + '.mp4', 'wb') as f:
+                f.write(data)
+            
+            with open(os.path.join(BASE_DIR+"/video_down",filename+'.mp4'), 'rb') as f:
+                data = f.read()
+            time.sleep(7)
+            response = HttpResponse(data, content_type='video/mp4')
+            response['Content-Disposition'] = "attachment; filename=%s" % file_hello
+            return response
+        else:
+            return render(request, 'index.html')
+    else:
+        return render(request, 'index.html')
 
 def download(request):
     if request.method == "POST":
