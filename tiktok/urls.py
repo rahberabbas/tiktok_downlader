c612d59354e62d735082902e7e2b895cf19f36c1
rahberabbas <amanabidi1234@gmail.com>
2021-08-13 16:32:46 +0530

    commited

--- tiktok/urls.py
+++ tiktok/urls.py
@@ -32,18 +32,20 @@ urlpatterns += i18n_patterns(
     path('faq/', faq, name='faq'),
     path('tiktok-video-cutter-online/', cutter, name="cutter"),
     path('tiktok-mp3-cutter-online/', audio_cutter, name="audio_cutter"),
+    path('download-tiktok-video-without-watermark/', without, name='without'),
+    path('tiktok-to-mp3-converter-online/', audio, name='audio'),
 
 
     # path('tiktok-download-with-watermark/', download, name='down'),
     # path('tiktok-download-with-watermark2/', download8, name='down8'),
-    path('tiktok-to-mp3-converter-online/', audio, name='audio'),
+    
     # path('tiktok-download-audio/', download1, name='down1'),
-    path('download-tiktok-video-without-watermark/', without, name='without'),
+    
     # path('tiktok-download-without-watermark/', download2, name='down2'),
     # path('tiktok-video-cutter-online-convert/', download3, name='down3'),
-    path('tiktok-cutter-link/', download4, name='down4'),
+    # path('tiktok-cutter-link/', download4, name='down4'),
     # path('tiktok-audio-cutter-online-convert/', download5, name='down5'),
-    path('tiktok-audio-cutter-link/', download6, name='down6'),
+    # path('tiktok-audio-cutter-link/', download6, name='down6'),
 )
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
